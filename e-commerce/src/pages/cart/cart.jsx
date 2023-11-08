440a2017b69b2010adf6a6993b272ea378bc3cd9
Brayan Hernandez <hbrayan0125@gmail.com>
2023-11-08 09:50:57 +0800

    added

---
+++ e-commerce/src/pages/cart/cart.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import { PRODUCTS } from "../../products";
+import { ShopContext } from "../../context/shop-context";
+import { CartItem } from "./cart-item";
+import "./cart.css"
+
+export const Cart = () => {
+    const {addToCart, cartItems} = useContext(ShopContext)
+    return(
+        <>
+            <div className="cart">
+                <div>
+                    <h1>
+                        Your Cart Items
+                    </h1>
+                </div>
+                <div className="cartItems">
+                    {
+                        PRODUCTS.map((product) => {
+                            if(cartItems[product.id] !== 0){
+                                return <CartItem data={product} />
+                            }
+                        })
+                    }
+                </div>
+                <div className="checkout">
+                    <p> Subtotal: $</p>
+                    <button> Continue Shopping </button>
+                    <button> Checkout </button>
+                </div>
+            </div>
+        </>
+    )
+}
